6c2ea5ebbba9bba2acf361133d12e498d9efc74b
AbrahamChumaceroGaite <abraham.chumacero@ucb.edu.bo>
2023-11-04 08:38:35 -0300

    presentation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   mapboxAccessToken: 'pk.eyJ1IjoiZGFudGVhYnJhaGFtMiIsImEiOiJjbGl1Mmg0dnMxamQ5M2ZudzB3bm01NnQ5In0.ElL-GieNaB6KaaIlsfeNKg',
-/*   apiUrl: 'https://weather-api.dev.404.codes/weather/api'  */
-  apiUrl: 'https://watchcloud.piensadiferente.net/weather/api'
+  apiUrl: 'https://weather-api.dev.404.codes/weather/api' 
+/*   apiUrl: 'https://watchcloud.piensadiferente.net/weather/api' */
 /*   apiUrl: 'http://localhost:3000/weather/api' */
   
 };
